ca7069ee1311db2620119beec55dbfd6739bd6d7
AirBorneJaws <bagratjames@gmail.com>
2021-10-22 17:31:16 -0700

    If this doesnt work

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,20 +8,20 @@ var voiceSelect = document.querySelector('select');
 function populateVoiceList()
 {
   voices = speechSynthesis.getVoices();
-  
+
   for (var i=0;i<voices.length;i++)
   {
     var option = document.createElement('option');
-    option.textContent=voices[i].name+'('+voices[i].lang+')';
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
 
     if (voices[i].default)
     {
       option.textContent += ' -- DEFAULT';
     }
 
-    option.setAttribute('data-lang',voices[i].lang);
-    option.setAttribute('data-name',voices[i].name);
-    voiceSelect.appendChild(soundOption);
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
   }
 }
 function init() {
