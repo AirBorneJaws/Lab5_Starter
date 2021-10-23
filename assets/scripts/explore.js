d90aeab8d129ff850a6dc7a70787707719482f7b
AirBorneJaws <bagratjames@gmail.com>
2021-10-22 17:41:05 -0700

    Final

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,32 +4,34 @@ window.addEventListener('DOMContentLoaded', init);
 var voices = [];
 var smilyFace = document.querySelector('img');
 var openSoundMenu = document.querySelector('select');
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
 
-function populateVoiceList()
-{
   voices = speechSynthesis.getVoices();
 
-  for (var i=0;i<voices.length;i++)
-  {
+  for(var i = 0; i < voices.length; i++) {
     var option = document.createElement('option');
     option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
 
-    if (voices[i].default)
-    {
+    if(voices[i].default) {
       option.textContent += ' -- DEFAULT';
     }
 
     option.setAttribute('data-lang', voices[i].lang);
     option.setAttribute('data-name', voices[i].name);
-    openSoundMenu.appendChild(option);
+    document.getElementById("voice-select").appendChild(option);
   }
 }
+
 function init() {
   // TODO
   populateVoiceList();
-  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
-  }
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
   var button = document.querySelector('button');
 
   button.addEventListener("click",function()
