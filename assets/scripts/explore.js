6b200a4a981372b7f935e0e87727a7ee65701f92
AirBorneJaws <bagratjames@gmail.com>
2021-10-22 17:14:42 -0700

    Explore Done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,56 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+var voices = [];
+var smilyFace = document.querySelector('img');
 
+function populateVoiceList()
+{
+  var openSoundMenu = document.getElementById("voice-select");
+  voices = speechSynthesis.getVoices();
+  for (var i=0;i<voices.length;i++)
+  {
+    var option = document.createElement('option');
+    option.textContent=voices[i].name+'('+voices[i].lang+')';
+
+    if (voices[i].default)
+    {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang',voices[i].lang);
+    option.setAttribute('data-name',voices[i].name);
+    openSoundMenu.appendChild(soundOption);
+  }
+}
 function init() {
   // TODO
+  populateVoiceList();
+
+  var button = document.querySelector('button');
+
+  button.addEventListener("click",function()
+  {
+    var inputText=document.getElementById('text-to-speak');
+    var voiceSelect = document.getElementById("voice-select");
+    var dataName = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    var utter = new SpeechSynthesisUtterance(inputText.value);
+
+    for (var i=0;i<voices.length;i++)
+    {
+      if(voices[i].name===dataName)
+      {
+        utter.voice=voices[i];
+      }
+    }
+    speechSynthesis.speak(utter);
+    if(speechSynthesis.speaking)
+    {
+      smilyFace.src='assets/images/smiling-open.png';
+    }
+    utter.addEventListener('end', function () 
+    {
+      smilyFace.src = 'assets/images/smiling.png';
+    })
+  });
 }
